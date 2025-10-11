3d5a03a1ced69a70d3165916a021bace158486f5
Ayush <dasayush483@gmail.com>
2025-10-12 01:30:16 +0530

    fixed sliding

---
+++ sliding/fixed.py
@@ -0,0 +1,10 @@
+maxsum =0
+curr = 0
+for i in range(k):
+    curr += arr[i]
+    maxsum = curr
+for i in range(k, len(arr)):
+    curr += arr[i] - arr[i-k]
+    maxsum = max(maxsum, curr)
+return maxsum
+    
