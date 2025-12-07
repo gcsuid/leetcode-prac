c9b01bc192b67c401deb3a63e9ff158cc33adddc
Ayush <dasayush483@gmail.com>
2025-12-07 12:58:14 +0530

    added input methods

--- sorting/bubblesort.py
+++ sorting/bubblesort.py
@@ -8,7 +8,25 @@ def bubbleSort(arr):
     return arr
 
 #example usage
-if __name__ == "__main__":
+'''if __name__ == "__main__":
     arr = [64, 34, 25, 12, 22, 11, 90]
     sorted_arr = bubbleSort(arr)
-    print("Sorted array is:", sorted_arr)
+    print("Sorted array is:", sorted_arr)'''
+
+#if taking input from user
+'''if __name__ == "__main__":
+    n = int(input("Enter number of elements: "))
+    arr = []
+    for i in range(n):
+        element = int(input(f"Enter element {i+1}: "))
+        arr.append(element)
+    
+    sorted_arr = bubbleSort(arr)
+    print("Sorted array is:", sorted_arr)'''
+
+
+#without main and f strings in the input:
+arr = list(map(int, input("Enter numbers separated by spaces: ").split()))
+sorted_arr = bubbleSort(arr)
+print("Sorted array is:", sorted_arr)
+
